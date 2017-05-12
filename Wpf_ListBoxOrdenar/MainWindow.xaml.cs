a17fd6b865856116852a90cdcae29ecf1851a772
zhuoIC <nicolimonar@gmail.com>
2017-05-12 09:41:35 +0200

    Añadiendo a GitHub

---
+++ Wpf_ListBoxOrdenar/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+//----------------------------------------------
+using System.ComponentModel;
+
+namespace Wpf_ListBoxOrdenar
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void cbxOrdenar_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Al seleccionar una opción
+            lbxMiListBox.Items.SortDescriptions.Clear(); // No hay ningún criterio de ordenación
+            if (cbxOrdenar.SelectedIndex == 0)
+            {
+                lbxMiListBox.Items.SortDescriptions.Add(new SortDescription("Content", ListSortDirection.Ascending));
+            }
+            if (cbxOrdenar.SelectedIndex == 1)
+            {
+                lbxMiListBox.Items.SortDescriptions.Add(new SortDescription("Content", ListSortDirection.Descending));
+            }
+        }
+    }
+}
